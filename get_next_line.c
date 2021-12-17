77ce711d37d9097b4adf69b976031815d199715e
Hong Ji Min <hongjimin@Hongui-MacBookPro.local>
2021-12-17 17:15:32 +0900

    git push origin main

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,35 @@
 /*   By: jihong <jihong@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 16:31:11 by jihong            #+#    #+#             */
-/*   Updated: 2021/12/17 15:48:45 by jihong           ###   ########.fr       */
+/*   Updated: 2021/12/17 17:10:00 by jihong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+#define BUFFER_SIZE 5
+
+static
+
+int	get_next_line(int fd, char **line)
+{
+	static char	buff[BUFFER_SIZE + 1];
+	int	flag;
+
+	if(fd < 0 || line == NULL || BUFFER_SIZE <= 0)
+		return (0);
+	flag = read(fd,buff,BUFFER_SIZE);
+	if (flag < 0)
+		return (0);
+	line = buff;
+	return (1);
+}
+
 int main(void)
 {
-
+	int fd;
+	char line[50][50];
+	fd = open("./test.txt",O_RDONLY);
+	if(get_next_line(fd,line) == 1)
+		printf("%s",line[0]);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,8 +6,14 @@
 /*   By: jihong <jihong@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 16:31:57 by jihong            #+#    #+#             */
-/*   Updated: 2021/12/17 15:49:52 by jihong           ###   ########.fr       */
+/*   Updated: 2021/12/17 17:04:47 by jihong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdio.h>
+
+#include <fcntl.h>
+
+#include<stdlib.h>
+#include<string.h>
