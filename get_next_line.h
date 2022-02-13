c05a47e33fecfc68904fb4a32f63a69a7bfe51cc
Jimin Hong <jihong@c3r2s7.42seoul.kr>
2022-02-13 14:56:25 +0900

    fix header

--- get_next_line.h
+++ get_next_line.h
@@ -6,19 +6,22 @@
 /*   By: jihong <jihong@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 16:31:57 by jihong            #+#    #+#             */
-/*   Updated: 2022/01/12 18:32:17 by jihong           ###   ########.fr       */
+/*   Updated: 2022/02/13 14:55:57 by jihong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-#include <stdio.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include <string.h>
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
 
 char	*get_next_line(int fd);
 size_t	ft_strlen(const char *c);
 char	*ft_strndup(char *str, size_t n);
-int	check_newline(char *str);
+int		check_newline(char *str);
 char	*ft_strjoin(char *str1, char *str2);
 char	*ft_strrchr(const char *str, int c);
+
+#endif
